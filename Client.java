3211f76faaf8fbd36f49c5bb0d9eb7b0302d6960
Ali <148272874+aliyasarman@users.noreply.github.com>
2024-07-05 11:31:51 -0400

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,103 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Client {
+    public static final String[] commands = {
+            "date and time",
+            "uptime",
+            "memory use",
+            "netstat",
+            "current users",
+            "running processes"
+    };
+
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the Network Address");
+        String networkAddress = in.nextLine();
+        System.out.println("Enter the port number.");
+        int port = in.nextInt();
+        int numberOfRequests = 0;
+
+        while (true) {
+            System.out.println("Please select on of the following options:\n" +
+                    "1: Date and Time\n" +
+                    "2: Uptime\n" +
+                    "3: Memory Use\n" +
+                    "4: Netstat\n" +
+                    "5: Current Users\n" +
+                    "6: Running Processes\n" +
+                    "0: Exit program");
+            int selection = in.nextInt();
+            if (selection < 0 || selection > 6) {
+                System.out.println("Invalid input: Please try again.");
+                continue;
+            }
+            if (selection == 0) {
+                break;
+            }
+            System.out.println("How many client requests will be generated? Choose between 1, 5, 10, 15, 20 and 25");
+            numberOfRequests = in.nextInt();
+            if (numberOfRequests > 25 || numberOfRequests < 1) {
+                System.out.println("Invalid amount of client requests. Choose between 1, 5, 10, 15, 20 and 25");
+                continue;
+            }
+            if (numberOfRequests != 1 && numberOfRequests % 5 != 0) {
+                System.out.println("Invalid amount of client requests. Choose between 1, 5, 10, 15, 20 and 25");
+                continue;
+            }
+
+            ExecutorService executor = Executors.newFixedThreadPool(numberOfRequests);
+            CompletionService<Long> completionService = new ExecutorCompletionService<>(executor);
+            long startTime = System.nanoTime();
+            for (int i = 0; i < numberOfRequests; i++) {
+                completionService.submit(() -> {
+                    // System.out.println("About to get response");
+                    try (Socket socket = new Socket(networkAddress, port)) {
+                        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        writer.println(commands[selection-1]);
+                        StringBuilder sb = new StringBuilder();
+                        String response;
+                        while ((response = reader.readLine()) != null) {
+                            sb.append(response).append("\n");
+                        }
+                      //  System.out.println(sb);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    long endTime = System.nanoTime();
+                    return endTime - startTime;
+                });
+                // System.out.println("Submitting " + i);
+            }
+
+            executor.shutdown();
+            //executor.awaitTermination(5000,TimeUnit.MILLISECONDS);
+
+            try {
+                long totalTurnAroundTime = 0;
+                for (int i = 0; i < numberOfRequests; i++) {
+                    Future<Long> future = completionService.take();
+                    long turnAroundTime = future.get();
+                    totalTurnAroundTime += turnAroundTime;
+                    System.out.println("Request " + (i + 1) + " turn-around time: " + TimeUnit.NANOSECONDS.toMillis(turnAroundTime) + " ms");
+                }
+                System.out.println();
+                System.out.println("Total turn-around time: " + TimeUnit.NANOSECONDS.toMillis(totalTurnAroundTime) + " ms");
+                System.out.println("Average turn-around time: " + (TimeUnit.NANOSECONDS.toMillis(totalTurnAroundTime / numberOfRequests) + " ms"));
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
---
+++ SingleThreaded.java
@@ -0,0 +1,103 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class SingleThreaded {
+    public static long startTime = System.currentTimeMillis();
+
+    public static void main(String[] args) {
+       int port = 7020;
+        Scanner in = new Scanner(System.in);
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server is listening on port 7020");
+            // curl http://localhost:7020
+
+            while (true) {
+                //System.out.println("test");
+                try (Socket socket = serverSocket.accept()) {
+                    System.out.println("New client connected");
+                    long processStartTime = System.currentTimeMillis();
+
+                    InputStream input = socket.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    String request = reader.readLine();
+                    String response = handleRequest(request);
+
+                    //System.out.println(response);
+
+                    OutputStream output = socket.getOutputStream();
+                    PrintWriter writer = new PrintWriter(output, true);
+                    writer.write(response);
+                    writer.flush();
+                    //long processEndTime = System.currentTimeMillis();
+                    //System.out.println("Process Request " +request + "time: " + (processEndTime-processStartTime));
+                } catch (IOException e) {
+                    System.out.println("Client exception: " + e.getMessage());
+                    e.printStackTrace();
+                }
+                // System.out.println("loop again");
+            }
+        } catch (IOException e) {
+            System.out.println("Server exception: " + e.getMessage());
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            System.out.println("Security exception: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static String handleRequest(String request) {
+        if (request == null) {
+            return "invalid request";
+        }
+        switch (request.toLowerCase()) {
+            case "date and time":
+                return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+            case "uptime":
+                return getUpTime();
+            case "memory use":
+                return getMemoryUse();
+            case "netstat":
+                return executeCommand("netstat");
+            case "current users":
+                return executeCommand("who");
+            case "running processes":
+                return executeCommand("ps -e");
+            default:
+                return "Invalid request";
+        }
+    }
+
+    private static String getUpTime() {
+        long uptime = System.currentTimeMillis() - startTime;
+        return String.format("%d min, %d sec",
+                TimeUnit.MILLISECONDS.toMinutes(uptime),
+                TimeUnit.MILLISECONDS.toSeconds(uptime) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(uptime)));
+    }
+
+    private static String getMemoryUse() {
+        Runtime runtime = Runtime.getRuntime();
+        long memory = runtime.totalMemory() - runtime.freeMemory();
+        return "Memory usage: " + memory / (1024 * 1024) + " MB";
+    }
+
+    private static String executeCommand(String command) {
+        StringBuilder output = new StringBuilder();
+        try {
+            Process process = Runtime.getRuntime().exec(command);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+}
